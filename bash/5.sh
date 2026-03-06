68c01818ff26dba39db5fce4206df1f0ed47ed81
Kaled Aljebur <kaledaljebur@gmail.com>
2026-03-07 03:21:20 +1000

    bash 5 updt

--- bash/5.sh
+++ bash/5.sh
@@ -5,7 +5,7 @@
 
 echo "Scanning hosts in the network..."
 
-for ip in 192.168.1.{1..5}
+for ip in 192.168.8.{1..5}
 do
     ping -c 1 $ip > /dev/null 2>&1
 
@@ -16,5 +16,14 @@ do
         echo "$ip is not reachable"
     fi
 done
+
+# for ip in 192.168.8.{1..5} >> ip value will be 192.168.8.1, 192.168.8.2, ..., 192.168.8.5
+# -c 1 >> send only 1 packet
+# > /dev/null >> discard normal output
+# 2>&1 >> discard error output
+# if [ $? -eq 0 ] >> Check the exit status of the previous command
+# -eq >> equal to
+# $? >> result of the last command
+# 0 >> success
 
 echo "Scan completed."
