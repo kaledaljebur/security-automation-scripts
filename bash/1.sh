0237ad553dbc81da3605627f9b090569e3df6839
Kaled Aljebur <kaledaljebur@gmail.com>
2026-03-06 23:51:11 +1000

    updt bash 1

--- bash/1.sh
+++ bash/1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Week 7 example: simple bash script
+# The below script is tested in Kali
+# https://github.com/kaledaljebur/security-automation-scripts
 
 echo "=== CYB803 Bash Security Automation Demo ==="
 echo "Current user: $USER"
@@ -9,12 +11,25 @@ echo "Date: $(date)"
 echo
 
 echo "Checking current logged-in users..."
-who
-echo
-
-echo "Checking listening network ports..."
-ss -tuln
+w
+# You may also use `who` in Ubuntu and Mint
 echo
 
 echo "Displaying last 5 failed login attempts (if available)..."
-grep "Failed password" /var/log/auth.log 2>/dev/null | tail -5
+
+journalctl | grep authentication
+# or `journalctl | grep authentication`, or `journalctl | grep pam`
+
+# For Ubuntu and LinuxMint, follow the below:
+# sudo apt update && sudo apt -y install openssh-server && sudo systemctl start ssh
+# `ssh localhost` then use wrong password
+# grep "Failed password" /var/log/auth.log 2>/dev/null | tail -5
+
+# For Alpine, use Hullu VM and SSH from Kali with wrong password
+# ssh root@AlpineIP
+# grep "Failed password" /var/log/messages
+
+echo "Checking listening network ports..."
+# In Kali and Mint make sure SSH is started `sudo systemctl start ssh`
+ss -tuln
+echo
