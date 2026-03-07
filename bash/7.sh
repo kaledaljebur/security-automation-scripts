8916d67a6772e599496b69ae1d6370cb7eb8969e
Kaled Aljebur <kaledaljebur@gmail.com>
2026-03-07 13:16:26 +1000

    bash 7 updt

--- bash/7.sh
+++ bash/7.sh
@@ -4,13 +4,6 @@
 # The below script is tested in Kali
 # https://github.com/kaledaljebur/security-automation-scripts
 
-# make sure to create 7-ips.txt with content like:
-# 192.168.8.1
-# 192.168.8.2
-# 192.168.8.3
-# 192.168.8.4
-# 192.168.8.5
-
 FILE="7-ips.txt"
 
 echo "Reading IP addresses from $FILE"
@@ -32,3 +25,15 @@ do
 done < $FILE
 
 echo "Host checking completed."
+
+
+# make sure to create 7-ips.txt with content like:
+# 192.168.8.1
+# 192.168.8.2
+# 192.168.8.3
+# 192.168.8.4
+# 192.168.8.5
+
+# read ip -> reads one line at a time from the input
+# ip -> stores the line value from $FILE
+# < $FILE will redirect the file as input to the while loop
