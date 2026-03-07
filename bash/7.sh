fff0891f6f8575437ec591c6aaca0271dd1bfd29
Kaled Aljebur <kaledaljebur@gmail.com>
2026-03-07 13:09:00 +1000

    bash 7 updt

--- bash/7.sh
+++ bash/7.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-FILE="ips.txt"
+# Files read example
+# The below script is tested in Kali
+# https://github.com/kaledaljebur/security-automation-scripts
+
+# make sure to create 7-ips.txt with content like:
+# 192.168.8.1
+# 192.168.8.2
+# 192.168.8.3
+# 192.168.8.4
+# 192.168.8.5
+
+FILE="7-ips.txt"
 
 echo "Reading IP addresses from $FILE"
 echo
