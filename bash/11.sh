9ef0056726b5d4b623f4907f694bb1cb3922fbfb
Kaled Aljebur <kaledaljebur@gmail.com>
2026-03-07 15:00:32 +1000

    bash 11 updt

--- bash/11.sh
+++ bash/11.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Automating System Administration Tasks example
+# The script below was tested in Kali
+# https://github.com/kaledaljebur/security-automation-scripts
+
 echo "Running system health checks..."
 echo
 
@@ -16,3 +20,17 @@ ps aux --sort=-%cpu | head -5
 echo
 
 echo "System check completed."
+
+
+# ps aux -> show all running processes
+# aux:
+#   a -> show processes for all users
+#   u -> display processes in user-oriented format (shows CPU, memory, user, etc.)
+#   x -> include processes not attached to a terminal (background services)
+
+# --sort -> tells ps how to sort the output
+# %cpu -> sort by CPU usage
+# - before %cpu -> descending order (highest first)
+# --sort=-%cpu -> sort processes by highest CPU usage
+
+# head -5 -> display the top 5 processes
