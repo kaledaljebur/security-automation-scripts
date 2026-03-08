73d014491c455ff8cc11f5ba54e65f831350b08f
Kaled Aljebur <kaledaljebur@gmail.com>
2026-03-08 16:22:26 +1000

    bash 15 updt

--- bash/15.sh
+++ bash/15.sh
@@ -9,35 +9,35 @@ set -e
 
 # Trap any error and display a message
 trap 'echo "An error occurred. The script will now exit."; exit 1' ERR
-
 echo "Starting security checks..."
 echo
 
 # ---- Example 1: Exit Code Checking ----
 echo "Ex1: Checking host availability..."
-
 if ping -c 1 192.168.1.10 > /dev/null 2>&1
 then
     echo "Host is reachable"
 else
     echo "Host is unreachable"
 fi
-
 echo
 
 # ---- Example 2: set -e demonstration (script stops if command fails) ----
+# This may fail if:
+#   the file does not exist
+#   the user does not have permission
+#   the destination directory cannot be written
+
 # echo "Ex2: Copying security log to backup directory..."
-
 # cp /var/log/auth.log /tmp/auth_backup.log
-
 # echo "Backup completed."
 # echo
 
 # ---- Example 3: Intentional error example ----
+# This will fail because /non_existing_directory is not exist
+
 # echo "Ex3: Testing trap error handler..."
-
 # ls /non_existing_directory
-
 # echo "This line will not execute if an error occurs."
 
 
