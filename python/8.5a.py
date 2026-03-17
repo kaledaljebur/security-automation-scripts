7f2b36e8bd7b5db6021eece53fa7e429ef36ec6b
Kaled Aljebur <kaledaljebur@gmail.com>
2026-03-18 01:17:25 +1000

    py 8.3-8.5

--- python/8.5a.py
+++ python/8.5a.py
@@ -1,7 +1,9 @@
 # Py sample: Threaded Chat Server 
 # The below script is tested in Kali & Windows
 # https://github.com/kaledaljebur/security-automation-scripts
-# To see the traffic, make sure to start Wireshark and select Loopback
+# To see the traffic, start Wireshark and select the Loopback interface
+# Use the filter: tcp.port == 5000 to show only your application traffic
+# and hide other unrelated background connections
 
 import socket          # import socket module for network communication
 import threading       # import threading to allow simultaneous send/receive
